75857e48564ff33f58796275ce1eafc9869cd5f2
ZoeWatson <32024786+ZoeWatson@users.noreply.github.com>
2018-06-08 09:55:03 -0400

    Add files via upload

--- sitemap.php
+++ sitemap.php
@@ -6,43 +6,11 @@
         <meta charset="utf-8">
         <meta name="author" content="Zoe Watson">
         <meta name="description" content="Class Prodject">
- <style type="text/css">
-            figure {
-                border: thin solid darkslategray;
-                border-radius: 0.4em;
-                padding: 0.5em;
-                text-align: center;
-            }
-
-            figcaption {
-                color: #839e99;
-                font-style: italic;
-                text-align: center;
-            }
-
-            img{
-                border-radius: 0.4em;
-                max-width: 100%
-            }
-
-            .right{
-                float: right;
-                margin-left: 1em;
-            }
-
-            .small {
-                width: 150px;
-            }
-        </style>
     </head>
 
     <body>
         <article>
         <h1>Zoe Watson</h1>
-        <figure class="right small">
-            <img src="images/Zwatson.jpg" alt="image of Zoe at computer class" >
-            <figcaption> my picture of Computer Class.</figcaption>
-        </figure>
         <ol>
             <li><a href="tryit.php">Try it page</a></li>
             <li><a href="sitemap.xml">XML Sitemap.xml</a></li>
